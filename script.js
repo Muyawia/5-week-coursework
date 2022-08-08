aa58c31f6798f7795b25449d6f5998ce3368682d
muyawia <muyawia_kashif@outlook.com>
2022-08-08 15:28:40 +0100

    set localstorage for monday to show in console

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-localStorage.setItem("Monday", "Eat Lasagna");
-localStorage.setItem("Tuesday", 0772540052);
-localStorage.setItem("Monday", "Eat Lasagna");
+//localStorage.setItem("Monday", "Eat Lasagna");
+//localStorage.setItem("Tuesday", 0772540052);
 //console.log(localStorage);
 //localStorage.removeItem("Monday");
 //console.log(localStorage);
-console.log(localStorage.key(1));
+//console.log(localStorage.key(1));
+
+var inValue1 = document.getElementById("inValue1");
+var insert1 = document.getElementById("Insert1");
+
+insert1.onclick = function (event) {
+    event.preventDefault()
+    var key = "Monday";
+    var value = inValue1.value;
+
+    localStorage.setItem(key, value);
+    console.log(localStorage);
+};
