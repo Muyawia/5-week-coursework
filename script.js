621c85c8326eaced1c8427414c9fc8b450a03039
muyawia <muyawia_kashif@outlook.com>
2022-08-08 14:58:15 +0100

    added starter code and localstorage testing

---
+++ script.js
@@ -0,0 +1,7 @@
+localStorage.setItem("Monday", "Eat Lasagna");
+localStorage.setItem("Tuesday", 0772540052);
+localStorage.setItem("Monday", "Eat Lasagna");
+//console.log(localStorage);
+//localStorage.removeItem("Monday");
+//console.log(localStorage);
+console.log(localStorage.key(1));
